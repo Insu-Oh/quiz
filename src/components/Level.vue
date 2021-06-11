dec8898a4ab59f65b4b1bb7959d57a91d62d22fa
insu <ohinsu134@gmail.com>
2021-06-11 10:27:37 +0900

    please work

---
+++ src/components/Level.vue
@@ -0,0 +1,28 @@
+// Selection component
+<template>
+  <div class="level">
+    <!-- title -->
+    <div class="title">Select your level</div>
+
+    <!-- Level and quantity inputs -->
+    <div class="inputs">
+      <div>
+        <label for="input2">QUANTITY</label>
+        <input type="text" placeholder="1/50" id="input2" v-model="$store.state.maxQuantity"> 
+      </div>
+    </div>
+
+    <!-- Enter buttons -->
+    <div class="buttons">
+    <router-link to="/quiz" :class="{hover: $store.state.isEffect}">START</router-link>
+    <router-link to="/" :class="{hover: $store.state.isEffect}">BACK TO HOME</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default ({
+
+})
+</script>
